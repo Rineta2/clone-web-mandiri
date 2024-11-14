b7ba72e668515e5fadf1d8216ba4ef95dfbd904c
Rineta2 <rzkir571@gmail.com>
2024-11-15 05:16:03 +0700

    create data faq

---
+++ src/components/ui/data/Faq.js
@@ -0,0 +1,43 @@
+import { FaChevronRight } from "react-icons/fa";
+
+export const faq = [
+  {
+    name: "Need for help?",
+    desc: "We are here to serve and solve your question."
+  }
+]
+
+export const faqBox = [
+  {
+    title: "Forgot ATM PIN",
+    desc: "What should I do if I forgot my Mandiri Debit card ATM PIN?",
+    name: "See Solution",
+    icons: <FaChevronRight />,
+    path: "/"
+  },
+
+  {
+    title: "Lost Mandiri Credit Card",
+    desc: "What should I do if my Mandiri Credit card is lost?",
+    name: "See Solution",
+    icons: <FaChevronRight />,
+    path: "/"
+  },
+
+  {
+    title: "Blocked Mandiri Debit Card",
+    desc: "What should I do if my Mandiri Debit card is blocked?",
+    name: "See Solution",
+    icons: <FaChevronRight />,
+    path: "/"
+  }
+]
+
+export const faqPrimary = [
+  {
+    name: "Not found the solution? Click here to further information.",
+    path: "/",
+    name: "See Other Inquiry",
+    icons: <FaChevronRight />,
+  },
+];
