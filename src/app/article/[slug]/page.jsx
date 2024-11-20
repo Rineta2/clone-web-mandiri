6121867f0f4ad57283134bffa780f9639b5689b1
Rineta2 <rzkir571@gmail.com>
2024-11-20 20:27:54 +0700

    article slug

--- src/app/article/[slug]/page.jsx
+++ src/app/article/[slug]/page.jsx
@@ -2,6 +2,10 @@ import React from "react";
 
 import { articleData } from "@/components/ui/data/Article";
 
+import Link from "next/link";
+
+import Image from "next/image";
+
 export default function ArticleDetails({ params }) {
   const { slug } = params;
 
@@ -12,9 +16,77 @@ export default function ArticleDetails({ params }) {
   }
 
   return (
-    <section>
-      <div className="container">
-        <h1>{article.title}</h1>
+    <section className="py-20">
+      <div className="max-w-[900px] mx-auto">
+        <div className="heading flex items-center gap-2 mb-10 mt-[-1rem]">
+          <Link href="/" className="text-[16px] text-primary">
+            Home
+          </Link>
+          <div>/</div>
+          <Link href={`/article/${article.slug}`} className="text-[16px]">
+            Berita
+          </Link>
+        </div>
+
+        {article.slugDetails.map((item) => {
+          return (
+            <div className="flex flex-col gap-10" key={item.id}>
+              <h1 className="text-[24px] text-center font-bold">
+                {item.title}
+              </h1>
+
+              <Image
+                src={item.img}
+                alt={item.title}
+                width={1000}
+                height={1000}
+                className="w-full h-auto"
+                quality={100}
+              />
+
+              <h3 className="text-[24px] font-bold text-secondary">
+                {item.hightlight}
+              </h3>
+
+              {item.text.map((item) => {
+                return <h3 key={item.id}>{item.title}</h3>;
+              })}
+
+              {item.content.map((item) => {
+                return (
+                  <div className="flex flex-col gap-10" key={item.id}>
+                    <h6 className="text-[20px] text-gray-600">{item.title}</h6>
+                    <p className="text-[14px]">{item.desc}</p>
+                  </div>
+                );
+              })}
+
+              {item.download.map((item) => {
+                return (
+                  <div className="" key={item.id}>
+                    <Link href={item.path} className="text-[18px] text-primary">
+                      {item.name}
+                    </Link>
+                  </div>
+                );
+              })}
+
+              <p className="text-[16px]">{item.desc}</p>
+
+              {item.link.map((item) => {
+                return (
+                  <ol className="flex flex-col gap-2" key={item.id}>
+                    <li className="list-disc text-[14px] ">
+                      <Link href={item.path} className="text-primary">
+                        {item.title}
+                      </Link>
+                    </li>
+                  </ol>
+                );
+              })}
+            </div>
+          );
+        })}
       </div>
     </section>
   );
--- src/components/ui/data/Article.js
+++ src/components/ui/data/Article.js
@@ -2,6 +2,7 @@ import img1 from "@/components/assest/article/img1.jpeg";
 import img2 from "@/components/assest/article/img2.jpeg";
 import img3 from "@/components/assest/article/img3.jpeg";
 import singeImg from "@/components/assest/article/logo-mandiri-digital.png"
+import img4 from "@/components/assest/article/img4.jpeg";
 
 export const articleData = [
   {
@@ -12,7 +13,50 @@ export const articleData = [
     title: "Pengumuman Relokasi (Pemindahan Alamat Jaringan Kantor) dan Penutupan Jaringan Kantor",
     desc: "Untuk memberikan pelayanan yang baik, dengan ini kami beritahukan pelaksanaan pengalihan dan atau pemindahan Kantor Cabang Pembantu (KCP)",
     name: "Selengkapnya",
-    slug: "pengumuman-relokasi-pemindahan-alamat-jaringan-kantor-dan-penutupan-jaringan-kantor"
+    slug: "pengumuman-relokasi-pemindahan-alamat-jaringan-kantor-dan-penutupan-jaringan-kantor",
+    slugDetails: [
+      {
+        id: 1,
+        title: "Pengumuman Relokasi (Pemindahan Alamat Jaringan Kantor) dan Penutupan Jaringan Kantor",
+        hightlight: "MANDIRI HIGHLIGHT",
+        desc: "Untuk memberikan pelayanan yang baik, dengan ini kami beritahukan pelaksanaan pengalihan dan atau pemindahan Kantor Cabang Pembantu (KCP) dengan klik detail informasi sebagai berikut:",
+        img: null,
+        link: [
+          {
+            id: 1,
+            title: "Penutupan Cabang 11 November 2024",
+            path: "#"
+          },
+
+          {
+            id: 2,
+            title: "Relokasi Cabang 4 November 2024",
+            path: "#"
+          },
+
+          {
+            id: 3,
+            title: "Relokasi Cabang 11 November 2024",
+            path: "#"
+          },
+
+          {
+            id: 4,
+            title: "Relokasi Cabang 18 November 2024",
+            path: "#"
+          },
+
+          {
+            id: 5,
+            title: "Relokasi Cabang 25 November 2024",
+            path: "#"
+          },
+        ],
+        text: [],
+        content: [],
+        download: null
+      }
+    ]
   },
 
   {
@@ -23,7 +67,70 @@ export const articleData = [
     title: "DAILY ECONOMIC AND MARKET REVIEW",
     desc: "Kinerja Penjualan Eceran Diperkirakan Meningkat pada Oktober 2024",
     name: "Selengkapnya",
-    slug: "daily-economic-and-market-review"
+    slug: "daily-economic-and-market-review",
+    slugDetails: [
+      {
+        id: 1,
+        title: "DAILY ECONOMIC AND MARKET REVIEW",
+        hightlight: "ECONOMIC REVIEW",
+        desc: null,
+        img: img2,
+        text: [
+          {
+            id: 1,
+            title: "DAILY ECONOMIC AND MARKET REVIEW"
+          },
+
+          {
+            id: 2,
+            title: "Office of Chief Economist, PT Bank Mandiri"
+          },
+
+          {
+            id: 3,
+            title: "November, 19, 2024 | Daily Economic Review: Utang Luar Negeri Meningkat 8,3% yoy Pada 3Q2024 "
+          }
+        ],
+        link: [],
+        content: [
+          {
+            id: 1,
+            title: "Posisi Utang Luar Negeri (ULN) Indonesia pada 3Q2024 tercatat sebesar USD 427,8 miliar.",
+            desc: "Angka tersebut meningkat 8,3% yoy dibandingkan dengan posisi ULN pada tahun sebelumnya yang sebesar USD 394,8 miliar. Perkembangan ULN tersebut terutama didorong oleh pertumbuhan utang sektor publik yang positif di 3Q2024."
+          },
+
+          {
+            id: 2,
+            title: "ULN Pemerintah pada 3Q2024 tumbuh 8,4% yoy.",
+            desc: "Posisi ULN Pemerintah pada 3Q2024 tercatat sebesar USD 204,1 miliar, naik USD 15,9 miliar dibandingkan periode 3Q2023 yang sebesar USD 188,3 miliar. Perkembangan ULN pemerintah ini dipengaruhi oleh penarikan pinjaman luar negeri dan peningkatan aliran masuk modal asing pada Surat Berharga Negara (SBN) domestik."
+          },
+          {
+            id: 3,
+            title: "ULN swasta menurun dibandingkan dengan periode yang sama tahun lalu.  ",
+            desc: "Posisi ULN swasta pada 3Q2024 terkontraksi 0,6% yoy, disebabkan oleh ULN lembaga keuangan (financial corporations) yang mengalami penurunan 3,2% yoy. Berdasarkan sektor ekonomi, ULN swasta terbesar berasal dari Sektor Industri Pengolahan, Jasa Keuangan dan Asuransi, Pengadaan Listrik dan Gas, serta Pertambangan dan Penggalian, dengan pangsa pasar mencapai 79,3% dari total ULN swasta."
+          },
+
+          {
+            id: 4,
+            title: "Struktur ULN Indonesia pada 3Q2024 tetap sehat dan terkendali.",
+            desc: "Hal ini tercermin dari rasio ULN Indonesia terhadap Produk Domestik Bruto (PDB) yang tetap terjaga di kisaran 31,1%, sedikit menurun dibandingkan dengan rasio pada kuartal sebelumnya sebesar 31,0%. Selain itu, struktur ULN Indonesia tetap sehat, ditunjukkan oleh ULN Indonesia yang tetap didominasi oleh ULN berjangka panjang, dengan pangsa mencapai 84,2% dari total ULN. Untuk menjaga agar struktur ULN tetap sehat, Bank Indonesia dan Pemerintah terus memperkuat koordinasi dalam pemantauan perkembangan ULN, serta menerapkan prinsip kehati-hatian dalam pengelolaan ULN. (sp)"
+          },
+
+          {
+            id: 5,
+            title: "Untuk informasi yang lebih lengkap, Report tersebut dapat Bapak/Ibu unduh pada website kami melalui link berikut ini:",
+            desc: null
+          }
+        ],
+        download: [
+          {
+            id: 1,
+            name: "Unduh Dokument Media",
+            path: "https://www.bankmandiri.co.id/documents/20143/0/20241119+-+Utang+Luar+Negeri+Meningkat+8%2C3_+yoy+Pada+3Q2024.pdf/350009ba-bbc5-3820-54f1-92235a8c227a?t=1731989083365"
+          }
+        ]
+      }
+    ],
   },
 
   {
@@ -34,7 +141,101 @@ export const articleData = [
     title: "Super! Bank Mandiri Bawa Solusi Perbankan Digital dan Reward Menarik bagi Diaspora di Houston, AS",
     desc: "Bank Mandiri terus berkomitmen untuk memperluas aksesibilitas layanan perbankan bagi masyarakat Indonesia, baik di dalam negeri maupun",
     name: "Selengkapnya",
-    slug: "super-bank-mandiri-bawa-solusi-perbankan-digital-dan-reward-menarik-bagi-diaspora-di-houston-as"
+    slug: "super-bank-mandiri-bawa-solusi-perbankan-digital-dan-reward-menarik-bagi-diaspora-di-houston-as",
+    slugDetails: [
+      {
+        id: 1,
+        title: "Super! Bank Mandiri Bawa Solusi Perbankan Digital dan Reward Menarik bagi Diaspora di Houston, AS",
+        hightlight: "NEWS & RELEASE",
+        desc: null,
+        img: img4,
+        text: [
+          {
+            id: 1,
+            title: "Houston, AS, 11 November 2024 - Bank Mandiri terus berkomitmen untuk memperluas aksesibilitas layanan perbankan bagi masyarakat Indonesia, baik di dalam negeri maupun mancanegara. Sebagai upaya mewujudkan hal tersebut, Bank Mandiri turut hadir dalam perhelatan Wonderful Indonesia Festival di Sugar Land Town Square, Houston, Texas."
+          },
+
+          {
+            id: 2,
+            title: "Festival ini tidak hanya menjadi ajang perayaan budaya, kuliner, dan kerajinan tangan Indonesia, tetapi juga menjadi platform bagi Bank Mandiri untuk memperkuat komitmennya menghadirkan solusi transaksi dan kemudahan bagi nasabah diaspora Indonesia melalui super app Livin’ by Mandiri."
+          },
+
+          {
+            id: 3,
+            title: "Direktur Jaringan & Retail Banking Bank Mandiri Aquarius Rudianto menyatakan, dalam kegiatan ini Bank Mandiri turut memperkenalkan program Livin’ Around the World (LATW) sebagai bentuk nyata dari komitmen perseroan untuk terus mendukung kebutuhan finansial diaspora Indonesia di luar negeri."
+          }
+        ],
+        link: [],
+        content: [
+          {
+            id: 1,
+            title: null,
+            desc: "“Melalui Livin’ by Mandiri, kami berupaya untuk mempermudah nasabah dalam melakukan transaksi perbankan secara digital, kapan saja dan di mana saja. Ini merupakan komitmen kami untuk terus berinovasi dan mendekatkan diri kepada nasabah, terutama diaspora Indonesia yang berada di mancanegara,” ujar Aquarius di sela-sela pembukaan Wonderful Indonesia Festival bersama dengan Konsul Jenderal Republik Indonesia di Houston, AS Ourina Ritonga, Sabtu (9/11) waktu setempat atau Minggu (10/11) waktu Indonesia Barat."
+          },
+
+          {
+            id: 2,
+            title: null,
+            desc: "Spesial dalam LATW di Houston, Bank Mandiri menghadirkan berbagai penawaran menarik yang dihadirkan oleh berbagai fitur menarik Livin’ by Mandiri, seperti Tabungan Now, Tabungan Valas/Multicurrency, Mandiri Tabungan Rencana dan Livin’ Investasi mulai dari Reksa Dana, SBN perdana hingga saham."
+          },
+
+          {
+            id: 3,
+            title: null,
+            desc: "Penyelenggaraan LATW Houston merupakan lanjutan dari rangkaian kegiatan Livin Around the World yang secara berkelanjutan telah dilaksanakan Bank Mandiri di lebih dari 18 negara dan program Mandiri Sahabatku dimana Bank Mandiri memberikan keterampilan keuangan dan kewirausahaan kepada diaspora dan Pekerja Migran indonesia, yang diharapkan dapat mencapai Kemandirian Finansial."
+          },
+
+          {
+            id: 4,
+            title: null,
+            desc: "Aquarius menambahkan, dengan semakin berkembangnya teknologi digital, Bank Mandiri terus berkomitmen untuk menghadirkan solusi yang relevan dan mendukung kebutuhan nasabah di luar negeri. Melalui Livin’ by Mandiri, nasabah dapat melakukan berbagai transaksi keuangan mulai dari pembukaan rekening, investasi, hingga transaksi valuta asing secara cepat dan aman."
+          },
+
+          {
+            id: 5,
+            title: null,
+            desc: "“Kehadiran super app ini diharapkan dapat menjadi jembatan antara Bank Mandiri dan diaspora Indonesia di berbagai belahan dunia,” imbuhnya."
+          },
+
+          {
+            id: 6,
+            title: null,
+            desc: "Terlebih, keberadaan diaspora Indonesia di Amerika Utara sangat penting bagi Bank Mandiri. Hingga Oktober 2024, tercatat lebih dari 3.500 pengguna terdaftar Livin’ by Mandiri di Amerika Serikat dan Kanada, dengan frekuensi mencapai lebih dari 176 ribu kali transaksi."
+          },
+
+          {
+            id: 7,
+            title: null,
+            desc: "Dalam kesempatan ini, Aquarius juga mengenalkan kembali layanan transfer valuta asing (valas) melalui aplikasi Livin' by Mandiri yang telah diperluas. “Untuk meningkatkan layanan bagi nasabah, Bank Mandiri menambahkan tujuh mata uang baru pada fitur transfer luar negeri meliputi Yen Jepang (JPY), Won Korea (KRW), Ringgit Malaysia (MYR), Franc Swiss (CHF), Dirham Uni Emirat Arab (AED), Dong Vietnam (VND), Peso Filipina (PHP),” terangnya.",
+          },
+
+          {
+            id: 8,
+            title: null,
+            desc: "Melalui inovasi ini, nasabah dapat melakukan transfer ke luar negeri dalam 17 mata uang asing yang mencakup hingga 180 negara, termasuk layanan pengiriman USD ke seluruh dunia. Dengan penambahan tujuh mata uang baru, nasabah memiliki lebih banyak opsi untuk melakukan transaksi, terutama bagi yang sering berhubungan dengan mitra bisnis atau keluarga di luar negeri.",
+          },
+
+          {
+            id: 8,
+            title: null,
+            desc: "Bukan hanya itu saja, Bank Mandiri juga memperluas layanan mata uang Tabungan Multicurrency yang kini tersedia dalam 14 pilihan mata uang. Hal tersebut juga yang menarik perhatian para mengunjung event International Expo di Hong Kong yang menghadirkan pengusaha UMKM dari Indonesia.",
+          },
+
+          {
+            id: 9,
+            title: null,
+            desc: "Sebagai informasi, sejak diluncurkan pada Oktober 2021, Livin’ by Mandiri telah menunjukkan pertumbuhan yang mengesankan. Hingga kuartal III 2024, pengguna aplikasi Livin' by Mandiri menembus angka 27,6 juta pengguna, naik 32% secara year on year (YoY). Adapun, dari jumlah tersebut, total nilai transaksi Livin' by Mandiri telah mencapai Rp2.940 triliun dengan frekuensi transaksi 2,7 miliar transaksi.",
+          },
+        ],
+        download: [
+          {
+            id: 1,
+            name: null,
+            path: "https://www.bankmandiri.co.id/documents/20143/0/20241119+-+Utang+Luar+Negeri+Meningkat+8%2C3_+yoy+Pada+3Q2024.pdf/350009ba-bbc5-3820-54f1-92235a8c227a?t=1731989083365"
+          }
+        ]
+      }
+    ],
   }
 ]
 
