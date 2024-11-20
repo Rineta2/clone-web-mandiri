11fae57b742c79f25d3172be886628f4fd047b1a
Rineta2 <rzkir571@gmail.com>
2024-11-20 19:35:50 +0700

    fixed home

--- src/components/ui/data/Home.js
+++ src/components/ui/data/Home.js
@@ -19,7 +19,7 @@ export const homeText = [
     title: "Bank Mandiri Berkomitmen Terapkan ESG",
     desc: "Komitmen untuk mencapai operasional dengan Net Zero Emission (NZE) pada tahun 2030, pembiayaan tahun 2060, dan memberdayakan kewirausahaan",
     name: "Selengkapnya",
-    href: "/esg",
+    href: "#",
     icons: <ChevronRight size={24} />,
   }
 ]
@@ -29,20 +29,20 @@ export const homeCard = [
     img: wallet,
     title: "Jelajahi keunggulan produk mandiri kartu kredit",
     icons: <ChevronRight size={24} />,
-    href: "https://www.mandirikartukredit.com/",
+    href: "#",
   },
 
   {
     img: creditCard,
     title: "Hubungan Investor",
     icons: <ChevronRight size={24} />,
-    href: "/hubungan-investor",
+    href: "#",
   },
 
   {
     img: livin,
     title: "Living a lively life with Livin' by Mandiri",
     icons: <ChevronRight size={24} />,
-    href: "/",
+    href: "#",
   }
 ];
